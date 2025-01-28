da4786a2c34c72c952291461f2f88adffbd8fdda
ElrohirGT <elrohirgt@gmail.com>
2025-01-27 21:30:36 -0600

    feat: Formatting

--- EJ2/p1.c
+++ EJ2/p1.c
@@ -1,50 +1,50 @@
 // Flavio Galán 22386
 
-#include <stdio.h>
-#include <unistd.h>
 #include <fcntl.h> // Used for open
+#include <stdio.h>
+#include <unistd.h> // Used for read and write
 
-int main(int argc, char* argv[]) {
-	// printf("Arg count: %d\n", argc);
+int main(int argc, char *argv[]) {
+  // printf("Arg count: %d\n", argc);
 
-	if (argc != 3) {
-		printf("Please supply a source file and a destination file!\n");
-		return 1;
-	}
+  if (argc != 3) {
+    printf("Please supply a source file and a destination file!\n");
+    return 1;
+  }
 
-	int srcFile = open(argv[1], O_RDONLY);
-	if (srcFile == -1) {
-		printf("Can't open src file!\n");
-		return 1;
-	}
+  int srcFile = open(argv[1], O_RDONLY);
+  if (srcFile == -1) {
+    printf("Can't open src file!\n");
+    return 1;
+  }
 
-	int destFile = open(argv[2], O_WRONLY);
-	if (destFile == -1) {
-		printf("Can't open destination file!\n");
-		return 1;
-	}
+  int destFile = open(argv[2], O_WRONLY);
+  if (destFile == -1) {
+    printf("Can't open destination file!\n");
+    return 1;
+  }
 
-	int bufferSize = 255;
-	int buffer[bufferSize];
-	ssize_t readResult = read(srcFile, &buffer, bufferSize);
+  int bufferSize = 255;
+  int buffer[bufferSize];
+  ssize_t readResult = read(srcFile, &buffer, bufferSize);
 
-	while (readResult > 0) {
-		ssize_t writeResult = write(destFile, &buffer, readResult);
+  while (readResult > 0) {
+    ssize_t writeResult = write(destFile, &buffer, readResult);
 
-		if (writeResult == -1) {
-			printf("An error occurred while writing to destination file!\n");
-			break;
-		} else {
-			readResult = read(srcFile, &buffer, bufferSize);
-		}
-	}
+    if (writeResult == -1) {
+      printf("An error occurred while writing to destination file!\n");
+      break;
+    } else {
+      readResult = read(srcFile, &buffer, bufferSize);
+    }
+  }
 
-	if (readResult == -1) {
-		printf("An error occurred while reading file bytes!\n");
-	}
+  if (readResult == -1) {
+    printf("An error occurred while reading file bytes!\n");
+  }
 
-	close(srcFile);
-	close(destFile);
+  close(srcFile);
+  close(destFile);
 
-	return 0;
+  return 0;
 }
